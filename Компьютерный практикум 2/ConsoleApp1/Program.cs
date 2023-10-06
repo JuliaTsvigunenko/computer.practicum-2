2d5240e51179bf5778c7bc4eca564523d2a3dfe7
JuliaTsvigunenko <122933386+JuliaTsvigunenko@users.noreply.github.com>
2023-10-07 02:32:48 +0300

    Add files via upload

---
+++ Компьютерный практикум 2/ConsoleApp1/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Компьютерный практиукм №2
+
+            //Задача 1
+
+            //double x1, y1, x2, y2, x3, y3, dlina1, dlina2, dlina3, P, S;
+
+            //Console.WriteLine("Введите координаты точек:");
+            //Console.Write("x1=");
+            //x1 = double.Parse(Console.ReadLine());
+            //Console.Write("y1=");
+            //y1 = double.Parse(Console.ReadLine());
+            //Console.Write("X2=");
+            //x2 = double.Parse(Console.ReadLine());
+            //Console.Write("y2=");
+            //y2 = double.Parse(Console.ReadLine());
+            //Console.Write("x3=");
+            //x3 = double.Parse(Console.ReadLine());
+            //Console.Write("y3=");
+            //y3 = double.Parse(Console.ReadLine());
+
+            //dlina1 = Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2));
+            //dlina2 = Math.Sqrt(Math.Pow(x3 - x2, 2) + Math.Pow(y3 - y2, 2));
+            //dlina3 = Math.Sqrt(Math.Pow(x3 - x1, 2) + Math.Pow(y3 - y1, 2));
+
+            //P = 0;
+            //P = (dlina1 + dlina2 + dlina3)/2;
+            //Console.WriteLine($"P={P}");
+
+            //S = Math.Sqrt(P * (P - dlina1) * (P - dlina2) * (P - dlina3));
+            //Console.WriteLine("S=" + S);
+
+            //Console.ReadKey();
+
+
+            //Задача 2
+
+            //Console.WriteLine("Введите расстояние поездки:");
+            //double s = double.Parse(Console.ReadLine());
+            //Console.WriteLine("Введите расход бензина на 100 км:");
+            //double b = double.Parse(Console.ReadLine());
+            //Console.WriteLine("Введите цену бензина за 1 литр:");
+            //double c = double.Parse(Console.ReadLine());
+            //double cost = (s / 100) * b * c;
+            //Console.WriteLine($"Стоимость поездки составит: {cost}");
+            //Console.ReadKey();
+
+
+            //Задача 3
+
+            //double bomboane, cookies, apples;
+
+            //Console.WriteLine("Введите цену за 1 кг конфет:");
+            //bomboane = double.Parse(Console.ReadLine());
+            //Console.WriteLine("Введите кол-во кг конфет:");
+            //double x = double.Parse(Console.ReadLine());
+            //Console.WriteLine("Введите цену за 1 кг печенья:");
+            //cookies = double.Parse(Console.ReadLine());
+            //Console.WriteLine("Введите кол-во кг печенья:");
+            //double y = double.Parse(Console.ReadLine());
+            //Console.WriteLine("Введите цену за 1 кг яблок:");
+            //apples = double.Parse(Console.ReadLine());
+            //Console.WriteLine("Введите кол-во кг яблок:");
+            //double z = double.Parse(Console.ReadLine());
+
+            //double summ = x * bomboane + y * cookies + z * apples;
+            //Console.WriteLine("Сумма покупки =" + summ);
+            //Console.ReadKey();
+
+
+
+
+
+
+
+        }
+    }
+}
